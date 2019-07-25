f26ea4dfcfcd1fd065e9c42e02a841322e4b5ae9
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-07-25 10:09:39 +0530

    Add explanatory comments

--- models/index.js
+++ models/index.js
@@ -3,6 +3,9 @@ const Sequelize = require('sequelize');
 let sequelize;
 
 if (process.env.DATABASE_URL) {
+  // For Heroku
+
+  // Initialize Sequelize using Postgres
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
@@ -14,13 +17,16 @@ if (process.env.DATABASE_URL) {
     },
   });
 } else {
+  // For local development and testing
+
+  // Initialize Sequelize using SQLite3
   sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './database.sqlite',
   });
 }
 
-
+// For Exporting
 const Post = require('./Post')(sequelize, Sequelize);
 
 module.exports = {
